efc5c4dfd3d0eecd1e97268074c8d0bfe1a94450
Anurag Swarnim Yadav <asy.msstate@gmail.com>
2024-07-30 10:48:49 -0500

    CodeT5BeamAnalysisResults

---
+++ CodeT5_Beam_Analysis/CodeT5_BeamTesting_DeDuplication/CodeT5RQ1_Seed43511/run_test.sh
@@ -0,0 +1,119 @@
+# python codet5_main.py \
+#     --num_beams 1 \
+#     --output_dir=./saved_models \
+#     --model_name=model.bin \
+#     --do_test \
+#     --test_data_file=./data/cve_fixes_test.csv \
+#     --encoder_block_size 512 \
+#     --decoder_block_size 256 \
+#     --eval_batch_size 1 \
+#     --n_gpu 1 \
+#     --seed 43511  2>&1 | tee beam1test.log
+# #
+# python codet5_main.py \
+#     --num_beams 2 \
+#     --output_dir=./saved_models \
+#     --model_name=model.bin \
+#     --do_test \
+#     --test_data_file=./data/cve_fixes_test.csv \
+#     --encoder_block_size 512 \
+#     --decoder_block_size 256 \
+#     --eval_batch_size 1 \
+#     --n_gpu 1 \
+#     --seed 43511  2>&1 | tee beam2test.log
+# #
+python codet5_main.py \
+    --num_beams 3 \
+    --output_dir=./saved_models \
+    --model_name=model.bin \
+    --do_test \
+    --test_data_file=./data/cve_fixes_test.csv \
+    --encoder_block_size 512 \
+    --decoder_block_size 256 \
+    --eval_batch_size 1 \
+    --n_gpu 1 \
+    --seed 43511  2>&1 | tee beam3test.log
+# #
+# python codet5_main.py \
+#     --num_beams 4 \
+#     --output_dir=./saved_models \
+#     --model_name=model.bin \
+#     --do_test \
+#     --test_data_file=./data/cve_fixes_test.csv \
+#     --encoder_block_size 512 \
+#     --decoder_block_size 256 \
+#     --eval_batch_size 1 \
+#     --n_gpu 1 \
+#     --seed 43511  2>&1 | tee beam4test.log
+# #
+# python codet5_main.py \
+#     --num_beams 5 \
+#     --output_dir=./saved_models \
+#     --model_name=model.bin \
+#     --do_test \
+#     --test_data_file=./data/cve_fixes_test.csv \
+#     --encoder_block_size 512 \
+#     --decoder_block_size 256 \
+#     --eval_batch_size 1 \
+#     --n_gpu 1 \
+#     --seed 43511  2>&1 | tee beam5test.log
+# #
+# python codet5_main.py \
+#     --num_beams 10 \
+#     --output_dir=./saved_models \
+#     --model_name=model.bin \
+#     --do_test \
+#     --test_data_file=./data/cve_fixes_test.csv \
+#     --encoder_block_size 512 \
+#     --decoder_block_size 256 \
+#     --eval_batch_size 1 \
+#     --n_gpu 1 \
+#     --seed 43511  2>&1 | tee beam10test.log
+# #
+# python codet5_main.py \
+#     --num_beams 15 \
+#     --output_dir=./saved_models \
+#     --model_name=model.bin \
+#     --do_test \
+#     --test_data_file=./data/cve_fixes_test.csv \
+#     --encoder_block_size 512 \
+#     --decoder_block_size 256 \
+#     --eval_batch_size 1 \
+#     --n_gpu 1 \
+#     --seed 43511  2>&1 | tee beam15test.log
+# #
+# python codet5_main.py \
+#     --num_beams 20 \
+#     --output_dir=./saved_models \
+#     --model_name=model.bin \
+#     --do_test \
+#     --test_data_file=./data/cve_fixes_test.csv \
+#     --encoder_block_size 512 \
+#     --decoder_block_size 256 \
+#     --eval_batch_size 1 \
+#     --n_gpu 1 \
+#     --seed 43511  2>&1 | tee beam20test.log
+# #
+# python codet5_main.py \
+#     --num_beams 50 \
+#     --output_dir=./saved_models \
+#     --model_name=model.bin \
+#     --do_test \
+#     --test_data_file=./data/cve_fixes_test.csv \
+#     --encoder_block_size 512 \
+#     --decoder_block_size 256 \
+#     --eval_batch_size 1 \
+#     --n_gpu 1 \
+#     --seed 43511  2>&1 | tee beam50test.log
+# #
+# python codet5_main.py \
+#     --num_beams 100 \
+#     --output_dir=./saved_models \
+#     --model_name=model.bin \
+#     --do_test \
+#     --test_data_file=./data/cve_fixes_test.csv \
+#     --encoder_block_size 512 \
+#     --decoder_block_size 256 \
+#     --eval_batch_size 1 \
+#     --n_gpu 1 \
+#     --seed 43511  2>&1 | tee beam100test.log
---
+++ CodeT5_Beam_Analysis/CodeT5_BeamTesting_Replication/CodeT5RQ1_Seed26312/run_train.sh
@@ -0,0 +1,17 @@
+python codet5_main.py \
+    --output_dir=./saved_models \
+    --model_name=model.bin \
+    --do_train \
+    --train_data_file=./data/cve_fixes_train.csv \
+    --eval_data_file=./data/cve_fixes_val.csv \
+    --test_data_file=./data/cve_fixes_test.csv \
+    --epochs 75 \
+    --encoder_block_size 512 \
+    --decoder_block_size 256 \
+    --train_batch_size 8 \
+    --eval_batch_size 8 \
+    --learning_rate 2e-5 \
+    --max_grad_norm 1.0 \
+    --n_gpu 1 \
+    --evaluate_during_training \
+    --seed 26312  2>&1 | tee train.log
