b004766a780d7034cf9caf91c187e598149beedf
Anurag Swarnim Yadav <asy.msstate@gmail.com>
2024-09-19 21:37:03 -0500

    sh files and code for VulReapir

--- RQ1/RQ1-Code/VulRepair/vulrepair_replication.py
+++ RQ1/RQ1-Code/VulRepair/vulrepair_replication.py
@@ -47,7 +47,7 @@ class TextDataset(Dataset):
             sources = val_data["source"].tolist()
             labels = val_data["target"].tolist()
         elif file_type == "test":
-            data = pd.read_csv("test-Unique-Entries.csv")
+            data = pd.read_csv(args.test_data_file)
             sources = data["source"]
             labels = data["target"]
         self.examples = []
@@ -124,7 +124,7 @@ def train(args, train_dataset, model, tokenizer, eval_dataset):
     tr_loss, logging_loss, avg_loss, tr_nb, tr_num, train_loss = 0.0, 0.0, 0.0, 0, 0, 0
     best_loss = 100
 
-    writer_path = "tb/codet5_training_loss"
+    writer_path = "./RQ1-Code/VulRepair/tb/codet5_training_loss"
     writer = SummaryWriter(writer_path)
 
     model.zero_grad()
@@ -269,14 +269,18 @@ def test(args, model, tokenizer, test_dataset, best_threshold=0.5):
     logger.info(f"Test Accuracy: {str(test_result)}")
 
     # write prediction to file
-    df = pd.DataFrame({"raw_predictions": [], "correctly_predicted": []})
-    df["raw_predictions"] = raw_predictions
-    df["correctly_predicted"] = accuracy
-    df.to_csv("./raw_predictions/VulRepair_raw_preds.csv")
+    filename = f"./RQ1-Code/VulRepair/raw_predictions/VulRepair_raw_preds_beam_{args.num_beams}.csv"
+    os.makedirs(os.path.dirname(filename), exist_ok=True)
+    df = pd.DataFrame({"raw_predictions": raw_predictions, "correctly_predicted": accuracy})
+    df.to_csv(filename)
 
 def main():
     parser = argparse.ArgumentParser()
     # Params
+    parser.add_argument("--train_data_file", default=None, type=str, required=False,
+                        help="The input training data file (a csv file).")
+    parser.add_argument("--test_data_file", default=None, type=str,
+                        help="An optional input evaluation data file to evaluate the perplexity on (a text file).")
     parser.add_argument("--output_dir", default=None, type=str, required=False,
                         help="The output directory where the model predictions and checkpoints will be written.")
     parser.add_argument("--model_type", default="t5", type=str,
@@ -331,12 +335,13 @@ def main():
                         help="random seed for initialization")
     parser.add_argument('--epochs', type=int, default=1,
                         help="training epochs")
+    parser.add_argument('--n_gpu', type=int, default=1,
+                        help="using which gpu")
 
     args = parser.parse_args()
 
     # Setup CUDA, GPU
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    args.n_gpu = 1
     args.device = device
 
     # Setup logging
@@ -353,11 +358,11 @@ def main():
     logger.info("Training/evaluation parameters %s", args)
     # Training
     if args.do_train:
-        train_data_whole = pd.read_csv("train-whole.csv")
+        train_data_whole = pd.read_csv(args.train_data_file)
         df = pd.DataFrame({"source": train_data_whole["source"], "target": train_data_whole["target"]})
         train_data, val_data = train_test_split(df, test_size=0.1238, random_state=42)
-        train_data.to_csv('train_data.csv', index=False)
-        val_data.to_csv('val_data.csv', index=False)
+        train_data.to_csv('./RQ1-Code/VulRepair/train_data.csv', index=False)
+        val_data.to_csv('./RQ1-Code/VulRepair/val_data.csv', index=False)
         train_dataset = TextDataset(tokenizer, args, train_data, val_data, file_type='train')
         eval_dataset = TextDataset(tokenizer, args, train_data, val_data, file_type='eval')
         train(args, train_dataset, model, tokenizer, eval_dataset)
