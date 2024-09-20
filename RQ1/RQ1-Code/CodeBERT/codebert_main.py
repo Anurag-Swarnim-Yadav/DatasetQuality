ed661697d7ed8cd061bca0a788592245618b08e7
Anurag Swarnim Yadav <asy.msstate@gmail.com>
2024-09-19 22:41:35 -0500

    CodeBERT code

--- RQ1/RQ1-Code/CodeBERT/codebert_main.py
+++ RQ1/RQ1-Code/CodeBERT/codebert_main.py
@@ -59,7 +59,7 @@ class TextDataset(Dataset):
             sources = val_data["source"].tolist()
             labels = val_data["target"].tolist()
         elif file_type == "test":
-            data = pd.read_csv("test-Unique-Entries.csv")
+            data = pd.read_csv(args.test_data_file)
             sources = data["source"]
             labels = data["target"]
         self.examples = []
@@ -268,16 +268,21 @@ def test(args, model, tokenizer, test_dataset, best_threshold=0.5):
     logger.info("***** Test results *****")
     logger.info(f"Test Accuracy: {str(test_result)}")
     
+    
     # write prediction to file
-    df = pd.DataFrame({"raw_predictions": [], "correctly_predicted": []})
-    df["raw_predictions"] = raw_predictions
-    df["correctly_predicted"] = accuracy
-    df.to_csv("./raw_predictions/CodeBERT_raw_preds.csv")
+    filename = f"./RQ1-Code/CodeBERT/raw_predictions/CodeBERT_raw_preds_beam_{args.beam_size}.csv"
+    os.makedirs(os.path.dirname(filename), exist_ok=True)
+    df = pd.DataFrame({"raw_predictions": raw_predictions, "correctly_predicted": accuracy})
+    df.to_csv(filename)
 
 
 def main():
     parser = argparse.ArgumentParser()
     # Params
+    parser.add_argument("--train_data_file", default=None, type=str, required=False,
+                        help="The input training data file (a csv file).")
+    parser.add_argument("--test_data_file", default=None, type=str,
+                        help="An optional input evaluation data file to evaluate the perplexity on (a text file).")
     parser.add_argument("--output_dir", default=None, type=str, required=False,
                         help="The output directory where the model predictions and checkpoints will be written.")
     ## Other parameters
@@ -346,10 +351,11 @@ def main():
                         help="random seed for initialization")
     parser.add_argument('--epochs', type=int, default=1,
                         help="training epochs")
+    parser.add_argument('--n_gpu', type=int, default=1,
+                        help="using which gpu")
     args = parser.parse_args()
     # Setup CUDA, GPU
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    args.n_gpu = 1
     args.device = device
 
     # Setup logging
@@ -373,11 +379,11 @@ def main():
     logger.info("Training/evaluation parameters %s", args)
     # Training
     if args.do_train:
-        train_data_whole = pd.read_csv("train-whole.csv")
+        train_data_whole = pd.read_csv(args.train_data_file)
         df = pd.DataFrame({"source": train_data_whole["source"], "target": train_data_whole["target"]})
         train_data, val_data = train_test_split(df, test_size=0.1238)
-        train_data.to_csv('train_data.csv', index=False)
-        val_data.to_csv('val_data.csv', index=False)
+        train_data.to_csv('./RQ1-Code/CodeBERT/train_data.csv', index=False)
+        val_data.to_csv('./RQ1-Code/CodeBERT/val_data.csv', index=False)
         train_dataset = TextDataset(tokenizer, args, train_data, val_data, file_type='train')
         eval_dataset = TextDataset(tokenizer, args, train_data, val_data, file_type='eval')
         train(args, train_dataset, model, tokenizer, eval_dataset)
@@ -389,7 +395,7 @@ def main():
         model.load_state_dict(torch.load(output_dir, map_location=args.device))
         model.to(args.device)
         test_dataset = TextDataset(tokenizer, args, file_type='test')
-        test(args, model, tokenizer, test_dataset, best_threshold=0.5)   
+        test(args, model, tokenizer, test_dataset, best_threshold=0.5)
     return results
 
 if __name__ == "__main__":
