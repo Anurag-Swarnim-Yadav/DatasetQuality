8ffa227dfb1c6c06b8aa77025836022c816f85dc
Anurag Swarnim Yadav <asy.msstate@gmail.com>
2024-09-22 21:51:05 -0500

    sh files

---
+++ RQ2A/run_VulRepair_train.sh
@@ -0,0 +1,18 @@
+python ./RQ1-Code/VulRepair/vulrepair_replication.py \
+    --output_dir=./RQ1-Code/VulRepair/VulRepair_model/saved_models \
+    --model_name=model.bin \
+    --tokenizer_name=Salesforce/codet5-base \
+    --model_name_or_path=Salesforce/codet5-base \
+    --do_train \
+    --train_data_file=./RQ1-Dataset/train-whole.csv \
+    --test_data_file=./RQ1-Dataset/test.csv \
+    --epochs 75 \
+    --encoder_block_size 512 \
+    --decoder_block_size 256 \
+    --train_batch_size 8 \
+    --eval_batch_size 8 \
+    --learning_rate 2e-5 \
+    --max_grad_norm 1.0 \
+    --n_gpu 1 \
+    --evaluate_during_training \
+    --seed 26312  2>&1 | tee ./RQ1-Code/VulRepair/VulRepair_train.log
---
+++ RQ3A/run_GraphCodeBERT_train.sh
@@ -0,0 +1,16 @@
+python ./RQ1-Code/GraphCodeBERT/graphcodebert_main.py \
+    --output_dir=./RQ1-Code/GraphCodeBERT/GraphCodeBERT_model/saved_models \
+    --model_name=model.bin \
+    --do_train \
+    --train_data_file=./RQ1-Dataset/train-whole.csv \
+    --test_data_file=./RQ1-Dataset/test.csv \
+    --epochs 75 \
+    --encoder_block_size 512 \
+    --decoder_block_size 256 \
+    --train_batch_size 8 \
+    --eval_batch_size 8 \
+    --learning_rate 2e-5 \
+    --max_grad_norm 1.0 \
+    --n_gpu 1 \
+    --evaluate_during_training \
+    --seed 26312  2>&1 | tee ./RQ1-Code/GraphCodeBERT/GraphCodeBERT_train.log
